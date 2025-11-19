73a520859d717e9023f9a7955403d750f6b8e2c4
hessam_khoobkar <amirhessam.dev@gmail.com>
2025-11-19 13:10:29 +0330

    Add Netlify configuration and switch adapter to Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { fileURLToPath } from 'url';
